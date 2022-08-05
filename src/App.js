b5a1c97c8e8e71d79df6af18b2e4d5b6268dd45c
Jason Yuan <jasonyuan0130@gmail.com>
2022-08-05 18:46:43 +0930

    add navbar

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Offers from "./pages/Offers"
 import Profile from "./pages/Profile"
 import SignIn from "./pages/SignIn"
 import SignUp from "./pages/SignUp"
+import Navbar from "./components/Navbar"
 
 const App = () => {
   return (
@@ -13,11 +14,12 @@ const App = () => {
         <Routes>
           <Route path="/" element={<Explore />} />
           <Route path="/offers" element={<Offers />} />
-          <Route path="/profile" element={<Profile />} />
+          <Route path="/profile" element={<SignIn />} />
           <Route path="/sign-in" element={<SignIn />} />
           <Route path="/sign-up" element={<SignUp />} />
           <Route path="/forgot-password" element={<ForgotPassword />} />
         </Routes>
+        <Navbar />
       </Router>
     </>
   )
